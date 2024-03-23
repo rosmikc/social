5ffe1857849b2098e3e4ca2f0c5356a5ba887536
Rosmi <rosmikc@gmail.com>
2024-03-23 10:49:45 +0900

    Adding buildSrc

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,13 @@
+object Modules {
+    const val app = ":app"
+
+    const val core = ":core"
+
+    const val onboardingDomain = ":onboarding:onboarding_domain"
+    const val onboardingPresentation = ":onboarding:onboarding_presentation"
+    const val onboardingData = ":onboarding:onboarding_data"
+
+    const val timelineData = ":timeline:timeline_data"
+    const val timelineDomain = ":timeline:timeline_domain"
+    const val timelinePresentation = ":timeline:timeline_presentation"
+}
