c7d25968bf6890e39191186af10c5e1503c61390
Rosmi <rosmikc@gmail.com>
2024-03-23 09:15:08 +0900

    Adding modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,12 @@ dependencyResolutionManagement {
 
 rootProject.name = "social"
 include(":app")
- 
+include(":core")
+include(":onboarding")
+include(":onboarding:onboarding_presentation")
+include(":onboarding:onboarding_domain")
+include(":onboarding:onboarding_data")
+include(":timeline")
+include(":timeline:timeline_presentation")
+include(":timeline:timeline_domain")
+include(":timeline:timeline_data")
